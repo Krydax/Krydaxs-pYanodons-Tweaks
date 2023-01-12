7adf06775bdf3b52e418d7ec8c5679cafeda9810
Krydax <93162034+Krydax@users.noreply.github.com>
2023-01-12 11:39:05 -0500

    order

--- settings.lua
+++ settings.lua
@@ -3,24 +3,28 @@ data:extend({
         type = "bool-setting",
         name = "extended-power-wires",
         setting_type = "startup",
-        default_value = true
+        default_value = true,
+        order = "aa"
     },
     {
         type = "bool-setting",
         name = "nerfs-and-buffs",
         setting_type = "startup",
-        default_value = true
+        default_value = true,
+        order = "ac"
     },
     {
         type = "bool-setting",
         name = "fluid-tank-rebalance",
         setting_type = "startup",
-        default_value = true
+        default_value = true,
+        order = "ab"
     },
     {
         type = "bool-setting",
         name = "more-expensive-loaders",
         setting_type = "startup",
-        default_value = true
+        default_value = true,
+        order = "ad"
     }
 })
